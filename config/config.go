cc5577813c73b89d5ef104489913bb07d78592d8
Umesh <globaltechumesh11@gmail.com>
2025-11-11 12:55:58 +0545

    Initial Step

--- config/config.go
+++ config/config.go
@@ -1,63 +1,3 @@
-// package config
-
-// import (
-// 	"os"
-// 	"strconv"
-// 	"sync"
-// )
-
-// type Config struct {
-// 	DBHost       string
-// 	DBUser       string
-// 	DBPassword   string
-// 	DBName       string
-// 	DBPort       string
-// 	ServerPort   string
-// 	JWTSecret    string
-// 	GoldProvider string
-// 	WorkerCount  int
-// 	QueueSize    int
-// }
-
-// var (
-// 	configInstance *Config
-// 	configOnce     sync.Once
-// )
-
-// func InitConfig() *Config {
-// 	configOnce.Do(func() {
-// 		configInstance = &Config{
-// 			DBHost:       getEnv("DB_HOST", "localhost"),
-// 			DBUser:       getEnv("DB_USER", "postgres"),
-// 			DBPassword:   getEnv("DB_PASSWORD", "postgres"),
-// 			DBName:       getEnv("DB_NAME", "gold_invest"),
-// 			DBPort:       getEnv("DB_PORT", "5432"),
-// 			ServerPort:   getEnv("PORT", "8080"),
-// 			JWTSecret:    getEnv("JWT_SECRET", "supersecretjwt"),
-// 			GoldProvider: getEnv("GOLD_PROVIDER_URL", "http://localhost:9000"),
-// 			WorkerCount:  getEnvAsInt("WORKER_COUNT", 5),
-// 			QueueSize:    getEnvAsInt("QUEUE_SIZE", 100),
-// 		}
-// 	})
-// 	return configInstance
-// }
-
-// func getEnv(key, defaultValue string) string {
-// 	if value := os.Getenv(key); value != "" {
-// 		return value
-// 	}
-// 	return defaultValue
-// }
-
-// func getEnvAsInt(key string, defaultValue int) int {
-// 	if value := os.Getenv(key); value != "" {
-// 		if intValue, err := strconv.Atoi(value); err == nil {
-// 			return intValue
-// 		}
-// 	}
-// 	return defaultValue
-// }
-
 package config
 
 import (
@@ -77,7 +17,6 @@ type Config struct {
 	GoldProvider string
 	WorkerCount  int
 	QueueSize    int
-	DatabaseURL  string // Add this for Railway
 }
 
 var (
@@ -90,15 +29,14 @@ func InitConfig() *Config {
 		configInstance = &Config{
 			DBHost:       getEnv("DB_HOST", "localhost"),
 			DBUser:       getEnv("DB_USER", "postgres"),
-			DBPassword:   getEnv("DB_PASSWORD", "password"), // Changed from "postgres"
-			DBName:       getEnv("DB_NAME", "gold_investment_db"),
+			DBPassword:   getEnv("DB_PASSWORD", "postgres"),
+			DBName:       getEnv("DB_NAME", "gold_invest"),
 			DBPort:       getEnv("DB_PORT", "5432"),
 			ServerPort:   getEnv("PORT", "8080"),
-			JWTSecret:    getEnv("JWT_SECRET", "dev_jwt_secret_min_32_chars_long_here"), // Better default
-			GoldProvider: getEnv("GOLD_PROVIDER_URL", ""),                               // Empty default
+			JWTSecret:    getEnv("JWT_SECRET", "supersecretjwt"),
+			GoldProvider: getEnv("GOLD_PROVIDER_URL", "http://localhost:9000"),
 			WorkerCount:  getEnvAsInt("WORKER_COUNT", 5),
 			QueueSize:    getEnvAsInt("QUEUE_SIZE", 100),
-			DatabaseURL:  getEnv("DATABASE_URL", ""),
 		}
 	})
 	return configInstance
--- config/database.go
+++ config/database.go
@@ -1,63 +1,3 @@
-// package config
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"sync"
-// 	"time"
-
-// 	"github.com/umesh/gold_investment/models"
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// )
-
-// var (
-// 	dbInstance *gorm.DB
-// 	dbOnce     sync.Once
-// )
-
-// type DB struct {
-// 	*gorm.DB
-// }
-
-// func ConnectDatabase(cfg *Config) *gorm.DB {
-// 	dbOnce.Do(func() {
-// 		dsn := fmt.Sprintf(
-// 			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-// 			cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort,
-// 		)
-
-// 		var err error
-// 		dbInstance, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-// 			PrepareStmt: true,
-// 		})
-// 		if err != nil {
-// 			log.Fatalf("Failed to connect to database: %v", err)
-// 		}
-
-// 		sqlDB, err := dbInstance.DB()
-// 		if err != nil {
-// 			log.Fatalf("Failed to get database instance: %v", err)
-// 		}
-
-// 		sqlDB.SetMaxIdleConns(10)
-// 		sqlDB.SetMaxOpenConns(100)
-// 		sqlDB.SetConnMaxLifetime(time.Hour)
-
-// 		log.Println("Database connected successfully")
-// 	})
-// 	return dbInstance
-// }
-
-// func AutoMigrate(db *gorm.DB) error {
-// 	return db.AutoMigrate(
-// 		&models.User{},
-// 		&models.Wallet{},
-// 		&models.Transaction{},
-// 		&models.GoldPrice{},
-// 	)
-// }
-
 package config
 
 import (
@@ -82,21 +22,10 @@ type DB struct {
 
 func ConnectDatabase(cfg *Config) *gorm.DB {
 	dbOnce.Do(func() {
-		log.Printf("Config - DatabaseURL set: %t", cfg.DatabaseURL != "")
-		log.Printf("Config - DBHost: %s", cfg.DBHost)
-		var dsn string
-
-		// Use DATABASE_URL if available (Railway), otherwise use individual config
-		if cfg.DatabaseURL != "" {
-			dsn = cfg.DatabaseURL
-			log.Println("Using DATABASE_URL for database connection")
-		} else {
-			dsn = fmt.Sprintf(
-				"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-				cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort,
-			)
-			log.Println("Using individual DB config for database connection")
-		}
+		dsn := fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort,
+		)
 
 		var err error
 		dbInstance, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
