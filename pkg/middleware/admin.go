ed97b8e80eb27feb20d204395f26c221778b5ab4
Umesh <globaltechumesh11@gmail.com>
2025-11-14 16:00:54 +0545

    Both Methods

--- pkg/middleware/admin.go
+++ pkg/middleware/admin.go
@@ -1,14 +1,15 @@
 package middleware
 
 import (
-	"database/sql"
 	"log"
 	"net/http"
 
+	"github.com/919Umesh/gold_go/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func AdminAuth(db *sql.DB) gin.HandlerFunc {
+func AdminAuth(db *gorm.DB) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		userId, exists := ctx.Get("user_id")
 
@@ -24,23 +25,25 @@ func AdminAuth(db *sql.DB) gin.HandlerFunc {
                WHERE id = ? 
                AND kyc_status = 'verified'
              `
-
 		var role string
-		err := db.QueryRow(query, userId).Scan(&role)
+
+		err := db.Raw(query, userId).Scan(&role).Error
 		if err != nil {
-			if err == sql.ErrNoRows {
-				ctx.JSON(http.StatusNotFound, gin.H{"error": "user not found or KYC not verified"})
-			} else {
-				log.Printf("Database error: %v", err)
-				ctx.JSON(http.StatusInternalServerError, gin.H{"error": "database error"})
-			}
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
 			ctx.Abort()
 			return
 		}
 		log.Print("----------Raw_Query-------------")
 		log.Print(role)
 
-		if role != "admin" {
+		var user models.User
+		if err := db.First(&user, userId).Error; err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "user not found"})
+			ctx.Abort()
+			return
+		}
+
+		if user.Role != "admin" {
 			ctx.JSON(http.StatusForbidden, gin.H{"error": "admin access required"})
 			ctx.Abort()
 			return
